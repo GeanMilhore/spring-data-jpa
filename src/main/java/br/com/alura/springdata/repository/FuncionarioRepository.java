2760a984f9b5a3c92d63559bfadb5e1320e4b60a
Gean <gean.milhore@gmail.com>
2022-11-26 03:11:04 -0300

    aula 3 - consulta com jpql @query + parametros

--- src/main/java/br/com/alura/springdata/repository/FuncionarioRepository.java
+++ src/main/java/br/com/alura/springdata/repository/FuncionarioRepository.java
@@ -1,5 +1,6 @@
 package br.com.alura.springdata.repository;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -13,12 +14,9 @@ import br.com.alura.springdata.orm.Funcionario;
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Long>{
 	List<Funcionario> findByNome(String nome);
 	
-	// método funcional porém desnecessáriamente verboso
-	//List<Funcionario> findByNomeAndSalarioGreaterThanAndDataContratacao(String nome, Double salario, LocalDate data);
-	
 	@Query("SELECT f FROM Funcionario f "
 			+ "WHERE f.nome = :nome "
 			+ "AND f.salario >= :salario "
 			+ "AND f.dataContratacao = :data ")
-	List<Funcionario> findNomeSalarioMaiorDataContratacao(String nome, Double salario, LocalDate data);
+	List<Funcionario> findNomeSalarioMaiorDataContratacao(String nome, BigDecimal salario, LocalDate data);
 }
--- src/main/java/br/com/alura/springdata/service/RelatorioService.java
+++ src/main/java/br/com/alura/springdata/service/RelatorioService.java
@@ -1,5 +1,8 @@
 package br.com.alura.springdata.service;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Scanner;
 
@@ -10,6 +13,8 @@ import br.com.alura.springdata.repository.FuncionarioRepository;
 
 @Service
 public class RelatorioService {
+	
+	private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
 	private final FuncionarioRepository repository;
 
@@ -23,6 +28,7 @@ public class RelatorioService {
 			System.out.println("[ Relatórios ] - Qual ação executar?");
 			System.out.println("0 - Voltar ao Menu Principal ");
 			System.out.println("1 - Buscar Funcionário pelo Nome ");
+			System.out.println("2 - Buscar Funcionário por Nome, Data contratação e Salario Maior ");
 
 			Integer action = scanner.nextInt();
 			switch (action) {
@@ -33,6 +39,9 @@ public class RelatorioService {
 			case 1:
 				buscarFuncionarioNome(scanner);
 				break;
+			case 2:
+				buscarFuncionarioNomeSalarioMaiorData(scanner);
+				break;
 
 			default:
 				System.out.println("Por favor selecione um opção valida...");
@@ -40,24 +49,39 @@ public class RelatorioService {
 			}
 		}
 	}
-	
+
 	private void buscarFuncionarioNome(Scanner scanner) {
-		
-		while(true) {
+
+		while (true) {
 			System.out.println("Digite 0 para voltar...");
 			System.out.println("Pesquisar por qual nome do funcionário?");
 			scanner.nextLine();
 			String name = scanner.nextLine();
 			List<Funcionario> list = repository.findByNome(name);
-			if(name.equals("0")){
+			if (name.equals("0")) {
 				System.out.println("Voltando...");
 				break;
 			}
-			if(list.isEmpty()) {
+			if (list.isEmpty()) {
 				System.out.println("Nenhum funcionário encontrado...");
 				continue;
 			}
 			list.forEach(System.out::println);
 		}
+	}
+
+	private void buscarFuncionarioNomeSalarioMaiorData(Scanner scanner) {
+		System.out.println("Qual nome deseja pesquisar?");
+		String nome = scanner.next();
+
+		System.out.println("Qual data contratação deseja pesquisar?");
+		String data = scanner.next();
+		LocalDate localDate = LocalDate.parse(data, formatter);
+
+		System.out.println("Qual salario deseja pesquisar?");
+		BigDecimal salario = new BigDecimal(scanner.nextDouble());
+		
+		List<Funcionario> list = repository.findNomeSalarioMaiorDataContratacao(nome, salario, localDate);
+		list.forEach(System.out::println);
 	}
 }
